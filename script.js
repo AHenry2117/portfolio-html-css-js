738950495f2579b506af7a0e02a08508c17b3b6c
AHenry2117 <105332403+AHenry2117@users.noreply.github.com>
2024-09-06 02:51:43 -0700

    Add files via upload
    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle for Navigation bar
+
+// function toggleMenu(){
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".hamburger-icon");
+//     menubar.classList.toggle("open");
+//     icon.classList.toggle("open");
+// }
